b07d286306a27ef5789f911f05aaf8d098e17b3b
White-OvO <Christophergonzalez1996@yahoo.com>
2023-01-04 16:01:11 -0800

    first commit

---
+++ src/GoodByeMoon.java
@@ -0,0 +1,53 @@
+
+public class GoodByeMoon 
+{ public static void main(String[] args) {
+System.out.println(ConsoleColors.CYAN+ "HEllo World");
+
+
+
+	System.out.println("hello world");
+	
+	
+	System.out.println( "My name is christopher");
+	
+	
+	int var1=26;
+
+	System.out.println("I am " + var1); 
+	System.out.println("i am looking for a cool color thatll work everyone to see my work");
+
+System.out.println(ConsoleColors.RESET + "Hello World we are back to normal color");
+System.out.println(ConsoleColors.FOREST_GREEN+ " Did i mention I am"+ " " + var1  );
+
+
+String firstname= "Christopher";
+String lastname= "Gonzalez";
+
+String fullname = firstname + " " + lastname ;
+
+System.out.println(fullname);
+
+System.out.println(ConsoleColors.RESET + "Class example 3");
+
+
+}
+public static void main2(String[] args) {
+
+int var;
+double x;
+
+var = 10;
+x = x /4;
+System.out.println("origional value of var: " + var);
+System.out.println("origional value of x: " + x);
+System.out.println();
+
+var = var / 4;
+x = x / 4;
+
+System.out.println("var after division: " + var);
+System.out.println("x after divsion: " + x);
+
+
+} //This is the last of main. 
+}
